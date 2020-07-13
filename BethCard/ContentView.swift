9c37e8a3839e5a56338febbb85670e396053cb8d
Beth-A <elizabeth78@me.com>
2020-07-13 13:26:42 -0400

    Initial Commit

---
+++ BethCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BethCard
+//
+//  Created by Mary Arnold on 7/13/20.
+//  Copyright © 2020 Beth Arnold. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

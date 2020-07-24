af4fc249578ebea1c9244fa138ed5e0b68c03860
Beth-A <elizabeth78@me.com>
2020-07-23 22:47:21 -0400

    Add 4 projects to portfolioView - buttons are not live yet

--- BethCard/PortfolioView.swift
+++ BethCard/PortfolioView.swift
@@ -14,18 +14,72 @@ struct PortfolioView: View {
     
     var body: some View {
         
-        ZStack {
-            Text("Page Two").font(.largeTitle).padding()
-            
-            Text("Go Back")
-                .padding()
-                .border(Color.primary)
-                .onTapGesture {
-                    self.env.currentPage = .Content
+        return ZStack {
+            AngularGradient(gradient: Gradient(colors: [.black, .black, .gray, .black, .black, .gray, .black]), center: .center)
+                .edgesIgnoringSafeArea(.all)
+            ScrollView {
+                VStack {
+                    Text("My Portfolio")
+                        .font(.largeTitle)
+                        .padding()
+                        .foregroundColor(.white)
+                    Group {
+                        VStack {
+                            Image("topSecretStamp")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 350, height: 225)
+                            
+                            InfoView(text: "Secrets - Web App", imageName: "desktopcomputer")
+                            //                            HStack{
+                            //                                InfoView(text: "View App", imageName: "")
+                            //                                InfoView(text: "View Code", imageName: "")
+                            //                            }
+                        }
+                    }
+                    .padding(.top, 20)
+                    
+                    Group {
+                        VStack {
+                            Image("todo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 350, height: 200)
+                                .padding(.bottom, 20)
+                            InfoView(text: "Todoey - iOS App", imageName:  "apps.iphone")
+                        }
+                    }
+                    .padding(.top, 20)
+                    Group {
+                        VStack {
+                            Image("natours")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 350, height: 150)
+                            //                                .padding(.bottom, 30)
+                            InfoView(text: "Natours - Web App", imageName: "desktopcomputer")
+                        }
+                    }
+                    .padding(.top, 20)
+                    Group {
+                        VStack {
+                            Image("movie")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 350, height: 275)
+                            //                                .padding(.bottom, 30)
+                            InfoView(text: "Movie Finder - Web App", imageName: "desktopcomputer")
+                        }
+                    }
+                    .padding(.top, 20)
+                }
             }
-        }.navigationBarBackButtonHidden(true)
+        }
     }
 }
+
+
+
 
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
